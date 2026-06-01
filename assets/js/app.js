a9839c5be39e5f5392c7a55019b33ef52b861747
azul <azulmengualtsdm@gmail.com>
2026-05-31 23:42:33 -0300

    feat:hice la card

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,31 @@ const personajes = [
     imagen: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg"
   }
 ];
+
+const rowPersonajes=document.querySelector('#personajes');
+const mostrarPersonajes=()=>{
+personajes.forEach((personaje)=> {
+    rowPersonajes.innerHTML+=`
+    <div class="col-3 m-3">
+          <div class="card" style="width: 18rem">
+            <img src="${personaje.imagen}" class="card-img-top" alt="${personaje.nombre}" />
+            <div class="card-body">
+              <h5 class="card-title">${personaje.nombre}</h5>
+              <a href="#" class="btn btn-eliminar bg-warning">Eliminar</a>
+            </div>
+          </div>
+        </div>
+    `
+}
+)
+}
+mostrarPersonajes();
+
+rowPersonajes.addEventListener('click', (e)=> {
+    if (e.target.classList.contains('btn-eliminar')) {
+         const card=e.target.closest('.card');
+         if (card){
+            card.remove()
+         }
+        }
+       });
